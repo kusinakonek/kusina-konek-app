5b5f244fe7c15b9c53d69f5d43aae2e611ea2ba0
JenoPro <148298044+JenoPro@users.noreply.github.com>
2026-02-19 17:07:06 +0800

    feat: Establish initial database schema and introduce feedback functionality with mobile UI and server-side logic.

--- packages/common/src/schemas/feedback.ts
+++ packages/common/src/schemas/feedback.ts
@@ -3,7 +3,8 @@ import { z } from "zod";
 export const createFeedbackSchema = z.object({
   disID: z.string().uuid(),
   ratingScore: z.number().int().min(1).max(5),
-  comments: z.string().min(1).optional()
+  comments: z.string().min(1).optional(),
+  photoUrl: z.string().optional()
 });
 
 export const updateFeedbackSchema = z.object({
